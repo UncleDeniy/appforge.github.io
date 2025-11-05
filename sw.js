771e275ad7049a59aa07edeaaa94da1605ef1236
Denis Krutko <denniskrutko@yandex.com>
2025-11-05 14:27:43 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-
-const CACHE='appforge-v21';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','/index.html','/apps.html','/os.html','/articles.html','/view.html','/assets/css/style.css','/assets/js/app.js'])))});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{let cl=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,cl)); return res;})).catch(()=>caches.match('/index.html')))});
+const C='fix-v1';self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['index.html'])))});self.addEventListener('activate',e=>self.clients.claim());self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(h=>h||fetch(e.request)))})
